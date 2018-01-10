fd892734ec4951ecdc72e0b9905b514108c157b2
Tom Straub <tstraub@cardinalsolutions.com>
2018-01-10 11:23:31 -0600

    Table output

--- scripts/network.sh
+++ scripts/network.sh
@@ -13,13 +13,13 @@ function get-ips-detailed {
   RG=$1
   echo "NIC List..."
   if [ -z ${RG} ]; then
-    az vm list-ip-addresses
+    az vm list-ip-addresses -o table
     echo "\nLoad Balanceers..."
     az network lb list -o table --query "[].{Name:name,PrivateIP:frontendIpConfigurations[0].privateIpAddress}"
     echo "\nPublic IPs..."
     az network public-ip list -o table --query '[].{Name:name,RG:resourceGroup,PublicIP:ipAddress}'
   else
-    az vm list-ip-addresses -g ${RG}
+    az vm list-ip-addresses -g ${RG} -o table
     echo "\nLoad Balanceers..."
     az network lb list -o table --query "[].{Name:name,PrivateIP:frontendIpConfigurations[0].privateIpAddress}" -g ${RG}
     echo "\nPublic IPs..."
